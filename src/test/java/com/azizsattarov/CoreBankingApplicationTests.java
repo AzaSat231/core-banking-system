945cd820007e2d8f0a7a67b402094998847b45bc
Aziz Sattarov <azizsattarov231@gmail.com>
2026-02-22 11:58:22 +0200

    Initial commit - core banking project structure

---
+++ src/test/java/com/azizsattarov/CoreBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azizsattarov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
